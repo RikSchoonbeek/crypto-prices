8dbd0d750c770ea59b5ab383c9daaed1ca0a345e
rikschoonbeek <rik.schoonbeek@gmail.com>
2019-05-05 08:09:02 +0200

    Added cryptocurrency, tickersymbol and exchange models

---
+++ crypto_prices/cryptodata/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class CryptoCurrency(models.Model):
+    name = models.CharField()
+    exchanges = models.ManyToManyField('Exchange')
+
+
+class TickerSymbol(models.Model):
+    symbol = models.CharField(max_length=8)
+    crypto = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE)
+
+
+class Exchange(models.Model):
+    name = models.CharField()
