5618b62e70278e3a9f4c3d16344b5a455a79d4f7
rikschoonbeek <rik.schoonbeek@gmail.com>
2019-05-05 17:08:28 +0200

    Added Exchange list and coinapi key to settings

--- crypto_prices/crypto_prices/settings.py
+++ crypto_prices/crypto_prices/settings.py
@@ -121,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Exchange Data
+EXCHANGES = [
+    "Kraken"
+]
+
+# api key for coinapi.io
+COINAPI_KEY = os.environ['COINAPI_KEY']
